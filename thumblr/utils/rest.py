e75528eb4a15cd334dc2ae5897290a7109a88846
Sergey Smirnov <smirnoffs@gmail.com>
2014-11-14 15:16:43 +0200

    rest_message answers with given http_status_code

--- thumblr/utils/rest.py
+++ thumblr/utils/rest.py
@@ -2,11 +2,13 @@ import json
 from django.http import HttpResponse
 
 
-def rest_message(status=None, **kwargs):
+def rest_message(status=None, http_status_code=200, **kwargs):
     d = {"status": status}
     d.update(kwargs)
 
-    return HttpResponse(
+    response = HttpResponse(
         json.dumps(d),
-        content_type='application/json'
+        content_type='application/json',
     )
+    response.status_code = http_status_code
+    return response
