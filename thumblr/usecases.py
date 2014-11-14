9582233fe329dfdc43a3f28c54e599ec86ce1044
Sergey Smirnov <smirnoffs@gmail.com>
2014-11-14 17:15:55 +0200

    updated  with model

--- thumblr/usecases.py
+++ thumblr/usecases.py
@@ -1,5 +1,7 @@
+from boto.sdb.db import model
 from thumblr.dto import ImageMetadata
-from thumblr.models import Image
+from thumblr.models import Image, ImageFile, ImageSize
+from thumblr.utils.hash import file_hash
 
 
 def add_image(image_file, image_metadata):
@@ -7,13 +9,25 @@ def add_image(image_file, image_metadata):
 
     image = Image()
 
-    image.file_name = image_metadata.file_name
-    image.file_type = image_metadata.file_type
-    image.site_id = image_metadata.site_id
-
-    image.object_content_type = image_metadata.content_type_id
+    image.original_file_name = image_metadata.original_file_name
+    image.site = image_metadata.site
+    image.content_type = image_metadata.content_type
     image.object_id = image_metadata.object_id
-
-    image.storage = image_file
-
     image.save()
+
+    image_file_inst = ImageFile()
+    image_file_inst.image = image
+    image_file_inst.image_in_storage = image_file
+    image_file_inst.image_hash = file_hash(image_file.file)
+
+    try:
+        original_size = ImageSize.objects.get(name='original')
+    except ImageSize.DoesNotExist:
+        original_size = ImageSize(name='original')
+
+    image_file_inst.size = original_size
+
+
+
+
+
