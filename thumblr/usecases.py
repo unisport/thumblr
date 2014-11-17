17bd6145595f5dbc3afdff31bea8941c004aeac1
Sergey Smirnov <smirnoffs@gmail.com>
2014-11-17 12:37:32 +0200

    hash image added to model and usecase

--- thumblr/usecases.py
+++ thumblr/usecases.py
@@ -4,6 +4,7 @@ from thumblr.models import Image, ImageFile, ImageSize
 from thumblr.utils.hash import file_hash
 
 
+@atomic
 def add_image(uploaded_file, image_metadata):
     assert isinstance(image_metadata, ImageMetadata)
 
@@ -18,7 +19,13 @@ def add_image(uploaded_file, image_metadata):
     image_file = ImageFile()
     image_file.image = image
     image_file.image_in_storage = uploaded_file
-    image_file.image_hash = file_hash(uploaded_file)
+
+    uploaded_file_hash = uploaded_file
+    hash_by_content = file_hash(uploaded_file)
+    uploaded_file_hash.name = hash_by_content
+
+    image_file.image_hash_in_storage = uploaded_file
+    image_file.image_hash = hash_by_content
 
     original_size = ImageSize.objects.get(name='original')
 
